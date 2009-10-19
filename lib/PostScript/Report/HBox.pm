9fa6a0ac529afd084ca71d2b7985a8a10eb34e56
Christopher J. Madsen <perl@cjmweb.net>
2009-10-19 15:17:54 -0500

    Don't use Moose::Autobox in HBox

--- lib/PostScript/Report/HBox.pm
+++ lib/PostScript/Report/HBox.pm
@@ -20,7 +20,7 @@ package PostScript::Report::HBox;
 our $VERSION = '0.01';
 
 use Moose;
-use Moose::Autobox;
+#use Moose::Autobox;
 use MooseX::Types::Moose qw(Bool Int Str);
 use PostScript::Report::Types ':all';
 
@@ -65,7 +65,7 @@ sub draw
 {
   my ($self, $x, $y, $rpt) = @_;
 
-  foreach my $child ($self->children->flatten) {
+  foreach my $child (@{ $self->children }) {
     $child->draw($x, $y, $rpt);
 
     $x += $child->width;
