1a023766a13361a9c6aca2d9915027322d5607e1
Christopher J. Madsen <perl@cjmweb.net>
2009-10-19 11:16:21 -0500

    Documented Container

--- lib/PostScript/Report/Role/Container.pm
+++ lib/PostScript/Report/Role/Container.pm
@@ -14,7 +14,7 @@ package PostScript::Report::Role::Container;
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the
 # GNU General Public License or the Artistic License for more details.
 #
-# ABSTRACT: Something that holds components
+# ABSTRACT: A component that has components
 #---------------------------------------------------------------------
 
 our $VERSION = '0.01';
@@ -28,6 +28,12 @@ use PostScript::Report::Types ':all';
 with 'PostScript::Report::Role::Component';
 
 my @inherited = (traits => [qw/TreeInherit/]);
+
+=attr-std children
+
+This is an arrayref containing the child Components.
+
+=cut
 
 has children => (
   metaclass => 'Collection::Array',
@@ -51,13 +57,35 @@ has padding_side => (
   @inherited,
 );
 
+=attr-std row_height
+
+(Inherited) This is used by VBoxes to set the height of children that
+don't specify their own height.  L<PostScript::Report::VBox>.
+
+=cut
+
 has row_height => (
   is        => 'ro',
   isa       => Int,
   @inherited,
 );
 
+=method draw
+
+The Container role provides an C<after draw> modifier to draw a border
+around the container.  The container must still provide its own C<draw>
+method.
+
+=cut
+
 after draw => \&draw_standard_border;
+
+=method init
+
+The Container role provides an C<after init> modifier that calls
+C<init> on each child Component.
+
+=cut
 
 after init => sub {
   my ($self, $parent, $report) = @_;
@@ -72,3 +100,32 @@ after init => sub {
 
 __END__
 
+=head1 DESCRIPTION
+
+This role describes a L<Component|PostScript::Report::Role::Component>
+that contains other Components.
+
+=begin Pod::Loom-group_attr std
+
+In addition to the
+L<standard attributes|PostScript::Report::Role::Component/ATTRIBUTES>,
+a Container provides the following:
+
+=head2 Inherited Attributes
+
+These attributes are provided so that child components can inherit
+them.  They have no effect on the container itself.
+L<PostScript::Report::Role::Component/"Optional Attributes">.
+
+=over
+
+=item padding_bottom
+
+=item padding_side
+
+=back
+
+=head2 Other Attributes
+
+=for Pod::Loom-omit
+CONFIGURATION AND ENVIRONMENT
